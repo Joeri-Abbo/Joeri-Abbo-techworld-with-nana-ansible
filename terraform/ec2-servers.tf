6fbe42375cd72d1e9aeee33ac8a066fc2aff8fb6
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-16 20:29:19 +0100

    20 - Dynamic Inventory for EC2 Servers

--- terraform/ec2-servers.tf
+++ terraform/ec2-servers.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "myapp-server-1" {
   key_name                    = aws_key_pair.ssh-key.key_name
 
   tags = {
-    Name : "${var.env_prefix}-server"
+    Name : "prod-server"
   }
 }
 
@@ -30,7 +30,7 @@ resource "aws_instance" "myapp-server-2" {
   key_name                    = aws_key_pair.ssh-key.key_name
 
   tags = {
-    Name : "${var.env_prefix}-server"
+    Name : "prod-server"
   }
 }
 
@@ -49,6 +49,24 @@ resource "aws_instance" "myapp-server-3" {
   key_name                    = aws_key_pair.ssh-key.key_name
 
   tags = {
-    Name : "${var.env_prefix}-server"
+    Name : "dev-server"
+  }
+}
+
+resource "aws_instance" "myapp-server-4" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id              = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [
+    aws_default_security_group.default-sg.id
+  ]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name : "dev-server"
   }
 }
