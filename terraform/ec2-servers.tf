ebfb8f6c091163298256feebb2ff4f05fb3edabc
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-16 20:22:58 +0100

    20 - Dynamic Inventory for EC2 Servers

--- terraform/ec2-servers.tf
+++ terraform/ec2-servers.tf
@@ -1,41 +1,4 @@
-#resource "aws_instance" "myapp-server-1" {
-#  ami           = var.ami
-#  instance_type = var.instance_type
-#
-#  subnet_id              = aws_subnet.myapp-subnet-1.id
-#  vpc_security_group_ids = [
-#    aws_default_security_group.default-sg.id
-#  ]
-#  availability_zone = var.avail_zone
-#
-#  associate_public_ip_address = true
-#  key_name                    = aws_key_pair.ssh-key.key_name
-#
-#  tags = {
-#    Name : "${var.env_prefix}-server"
-#  }
-#}
-#
-#resource "aws_instance" "myapp-server-2" {
-#  ami           = var.ami
-#  instance_type = var.instance_type
-#
-#  subnet_id              = aws_subnet.myapp-subnet-1.id
-#  vpc_security_group_ids = [
-#    aws_default_security_group.default-sg.id
-#  ]
-#  availability_zone = var.avail_zone
-#
-#  associate_public_ip_address = true
-#  key_name                    = aws_key_pair.ssh-key.key_name
-#
-#  tags = {
-#    Name : "${var.env_prefix}-server"
-#  }
-#}
-
-
-resource "aws_instance" "myapp-server-3" {
+resource "aws_instance" "myapp-server-1" {
   ami           = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
@@ -53,13 +16,39 @@ resource "aws_instance" "myapp-server-3" {
   }
 }
 
-resource "null_resource" "configure_server" {
+resource "aws_instance" "myapp-server-2" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
 
-  triggers = {
-    trigger = aws_instance.myapp-server-3.public_ip
+  subnet_id              = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [
+    aws_default_security_group.default-sg.id
+  ]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name : "${var.env_prefix}-server"
   }
-  provisioner "local-exec" {
-    working_dir = "/Users/joeriabbo/Sites/devops/techworld-with-nana/Joeri-Abbo-techworld-with-nana-ansible"
-    command     = "ansible-playbook --inventory ${aws_instance.myapp-server-3.public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-new.yaml"
+}
+
+
+resource "aws_instance" "myapp-server-3" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id              = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [
+    aws_default_security_group.default-sg.id
+  ]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    Name : "${var.env_prefix}-server"
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cider_block
-  tags       = {
+  cidr_block           = var.vpc_cider_block
+  enable_dns_hostnames = true
+  tags                 = {
     Name = "${var.env_prefix}-vpc"
   }
 }
